420f04008910353b06db3341efde5cc75fbd253d
Vu Tran <tranvietvu0218@gmail.com>
2023-12-09 18:46:23 +0700

    update router admin_users

--- app/Models/User.js
+++ app/Models/User.js
@@ -6,14 +6,17 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
+      unique: true
     },
     email: {
       type: String,
       required: true,
+      unique: true
     },
     phone: {
       type: String,
       required: true,
+      unique: true
     },
     password: {
       type: String,
